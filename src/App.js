b57ba0cbfbd27824e3e4342b16b1b8edbe80e24f
John <john.thom.gray@gmail.com>
2024-05-30 14:04:02 -0700

    added landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Home';
+import Home from './components/Home';
 
 function App() {
   return (
--- src/Home.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-        <main>
-            <div>
-                <p>Home Page</p>
-            </div>
-        </main>
-    )
-}
