18b84e348da9e105e59c154a8476d600fee4225d
John <john.thom.gray@gmail.com>
2024-06-03 13:00:26 -0700

    landing re-added

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,7 +2,29 @@ export default function Landing() {
     return (
         <section id="landing-section">
             <div>
+                <div>
+                    <div>
+                        <h1>John Gray</h1>
+                        <h2>Full-stack Developer</h2>
+                        <p>A really nice guy!</p>
+                        <div>
+                            <a href="https://www.linkedin.com/in/john-thomas-gray" target="_blank" rel="noopener noreferrer">
+                                <img src="img/linkedin.png" alt="linkedin icon" width="22px" />
+                            </a>
+                            <a href="https://github.com/john-thomas-gray" target="_blank" rel="noopener noreferrer">
+                                <img src="img/github.png" alt="github icon" width="22px" />
+                            </a>
+                            <a href="mailto:johnthomgray@gmail.com" target="_blank" rel="noopener noreferrer">
+                                <img src="img/mail.png" alt="mail icon" width="22px" />
+                            </a>
+                        </div>
+                        <div>
+                            <img src="./img/johngray.png" alt="Profile pic" width="200px" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
 }
+
